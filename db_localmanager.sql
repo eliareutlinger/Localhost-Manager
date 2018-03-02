d4523cecb1aeceaed76e28fa28b48f0f3da5f548
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-03-02 09:04:44 +0100

    Updated DB-Import
    Added CREATE DATABASE statement & changed name

--- db_localmanager.sql
+++ db_localmanager.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 23. Nov 2017 um 22:36
--- Server-Version: 10.1.25-MariaDB
--- PHP-Version: 7.1.7
+-- Erstellungszeit: 02. Mrz 2018 um 09:03
+-- Server-Version: 10.1.30-MariaDB
+-- PHP-Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `db_localmanager`
 --
+CREATE DATABASE IF NOT EXISTS `db_localmanager` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db_localmanager`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +30,6 @@ SET time_zone = "+00:00";
 -- Tabellenstruktur für Tabelle `tb_file_data`
 --
 
-DROP TABLE IF EXISTS `tb_file_data`;
 CREATE TABLE `tb_file_data` (
   `ID` int(11) NOT NULL,
   `file_name` varchar(255) NOT NULL,
@@ -36,20 +37,6 @@ CREATE TABLE `tb_file_data` (
   `file_reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `github` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `tb_file_data`
---
-
-INSERT INTO `tb_file_data` (`ID`, `file_name`, `file_description`, `file_reg_date`, `github`) VALUES
-(58, 'busse.eliareutlinger.ch', '', '2017-11-23 09:34:08', 0),
-(59, 'EliaReutlinger.ch', '', '2017-11-23 09:34:08', 0),
-(60, 'M151-Project', '', '2017-11-23 09:34:08', 0),
-(61, 'Modul-133', '', '2017-11-23 09:34:08', 0),
-(62, 'Modul-151', '', '2017-11-23 09:34:08', 0),
-(63, 'PHP-Weight-Manager', '', '2017-11-23 09:34:08', 0),
-(64, 'Stundenplan-App', '', '2017-11-23 09:34:08', 0),
-(65, 'Web-CMS', '', '2017-11-23 09:34:08', 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -69,7 +56,8 @@ ALTER TABLE `tb_file_data`
 -- AUTO_INCREMENT für Tabelle `tb_file_data`
 --
 ALTER TABLE `tb_file_data`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;COMMIT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
